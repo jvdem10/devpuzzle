c5cdcfcfc61d49f2b5d17709bae7b5b9ddb20ee8
João Victor Demarco <joaodemarco10@gmail.com>
2026-03-11 00:51:33 -0300

    feat(migrations): add migrations for `tech` entity

---
+++ database/migrations/2026_03_11_024010_create_techs_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('techs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('image_path');
+            $table->boolean('open_source');
+            $table->date('release_date');
+            $table->string("creator_type");
+            $table->timestamps();
+        });
+
+        // Define the tech type(s)
+        Schema::create('tech_type', function (Blueprint $table) {
+            $table->id();
+            $table->foreignid('tech_id')->constrained('techs');
+            $table->foreignid('type_id')->constrained('tech_types');
+        });
+
+        // Define the tech area(s)
+        Schema::create('tech_area', function (Blueprint $table) {
+            $table->id();
+            $table->foreignid('tech_id')->constrained('techs');
+            $table->foreignid('area_id')->constrained('tech_areas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tech_type');
+        Schema::dropIfExists('tech_area');
+        Schema::dropIfExists('techs');
+    }
+};
